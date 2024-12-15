884237c92ad722b9fd45ece37fa1ad43d0760a0c
ErikBorgogno <erikborgo.20@gmail.com>
2024-12-15 10:12:35 +0100

    add function index

--- app/Http/Controllers/api/ProjectController.php
+++ app/Http/Controllers/api/ProjectController.php
@@ -3,9 +3,13 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(){
+        $project = Project::all();
+        return response()->json(['projects' => $project]);
+    }
 }
