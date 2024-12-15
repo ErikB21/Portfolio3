5536616dea6b1b0c5134ad459b2b8ee4f2209b36
ErikBorgogno <erikborgo.20@gmail.com>
2024-12-15 09:52:42 +0100

    add relationship with project

--- app/Models/Skill.php
+++ app/Models/Skill.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Skill extends Model
 {
@@ -15,5 +16,10 @@ class Skill extends Model
     public function user():BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function projects():BelongsToMany
+    {
+        return $this->belongsToMany(Project::class, 'project_skill');
     }
 }
