8ec1ed5ef78d6f7a13ecfcfc349a81998efffff1
ErikBorgogno <erikborgo.20@gmail.com>
2024-12-15 09:51:49 +0100

    add migration for Project

---
+++ database/migrations/2024_12_15_083403_create_projects_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('path_image');
+            $table->string('name');
+            $table->mediumText('text');
+            $table->string('url');
+            $table->string('url_video');
+            //mi serve una che si lega alla tabella skills, in quanto ogni progetto è creato con delle skills
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
