60232a8c8c8b95dcac9d43f095f7bc63f0f23dc3
ErikBorgogno <erikborgo.20@gmail.com>
2024-12-19 12:27:10 +0100

    modify layout

--- resources/js/pages/Projects.vue
+++ resources/js/pages/Projects.vue
@@ -1,21 +1,15 @@
 <template>
-    <div class="container-fluid d-flex flex-column justify-content-between px-0 eb_bg">
-        <h1 class="text-center text-warning pt-4">My Projects</h1>
+    <div class="container-fluid eb_bg">
+        <h1 class="text-center fw-bold my-5">Projects</h1>
         <div class="row justify-content-center">
-            <div class="col-12-col-md-6 col-lg-4 mt-4" style="width:22rem;">
-                <div class="card">
-                    <img src="#" class="card-img-top" style="height:12rem" alt="#">
-                    <div class="card-body">
-                        <h5 class="card-title">Card.name</h5>
-                        <p class="card-text">Card.text</p>
-                        <hr>
-                        <div class="d-flex flex-wrap">
-                            <span class="badge my-1 mx-2 py-2 rounded-pill">tag.Lang</span>
-                        </div>
-                        <hr>
-                        <a href="#" class="btn card-text">Watch</a>
-                    </div>
+            <!-- Aggiungi i CardProject all'interno di una colonna -->
+            <div class="books-container d-flex flex-column justify-content-center">
+                <div class="books d-flex">
+                    <CardProject v-for="(project, index) in projects" :key="index" :project="project"/>
                 </div>
+
+                <!-- Mensola sotto i progetti -->
+                <div class="shelf"></div>
             </div>
         </div>
     </div>
@@ -23,75 +17,72 @@
 
 <script>
 import axios from 'axios';
+import CardProject from '../components/CardProject.vue';
+
 export default {
     name: 'Projects',
-    data(){
-        return{
+    components: { CardProject },
+    data() {
+        return {
             projects: [],
             error: null,
-            colors: [
-
-            ]
         };
     },
-    mounted(){
+    mounted() {
         this.getProjects();
     },
     methods: {
-        async getProjects(){
-            try{
+        async getProjects() {
+            try {
                 const response = await axios.get('/api/project');
-                console.log('Response:', response.data);
                 this.projects = response.data.projects;
-                console.log(this.projects);
-            } catch(error){
-                console.error('Error fetching projects:', error);
-                this.error = 'Impossibile caricare i progetti. Riprova più tardi!'
+            } catch (error) {
+                this.error = 'Impossibile caricare i progetti. Riprova più tardi!';
             }
-        }
-    }
-}
+        },
+    },
+};
 </script>
 
-
-
 <style scoped lang="scss">
-.eb_bg{
-    h1{
-        font-family: 'Press Start 2P', cursive;
-        font-size: 4rem;
+.eb_bg {
+    h1 {
+        font-size: 4.5rem;
+        color: #7f5af0;
     }
-
-    h5{
-        font-size: 2.5rem;
-        font-family: 'Nanum Pen Script', cursive;
-    }
-    .btn{
-        background-color: #3B576B;
+    .btn {
+        background-color: #3b576b;
         color: whitesmoke;
-        &:hover{
+        &:hover {
             background-color: black;
         }
     }
-    span{
-        font-size: 0.8rem;
-    }
-    .bg-purple{
-        background-color: #712cf9;
-        color: white;
-    }
-    .bg-violet{
-        background-color: violet;
-        color: white;
-    }
 }
 
-@media screen and (max-width:575.98px) {
-    .eb_bg{
-        background-image: none;
-        h1{
-            font-size: 2.5rem;
-        }
-    }
+/* Wrapper che contiene tutti i libri (CardProject) */
+.books-container {
+    display: flex;
+    flex-direction: column; /* Layout verticale per i libri */
+    width: 100%;
+}
+
+/* Contenitore dei libri */
+.books {
+    display: flex;
+    flex-direction: row; /* Disporre i libri in verticale */
+    gap: 10px;
+    width: 100%;
+    height: 200px;
+}
+
+/* Mensola */
+.shelf {
+    width: 100%;
+    height: 30px;
+    background-image: url('/images/wood.png');
+    background-size: cover;
+    background-repeat: no-repeat;
+    border-radius: 0 0 8px 8px;
+    position: relative;
 }
 </style>
