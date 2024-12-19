567243e3cf31adb24b40fcb14195d9d9ac8224be
ErikBorgogno <erikborgo.20@gmail.com>
2024-12-19 12:24:40 +0100

    add function show for each project

--- app/Http/Controllers/api/ProjectController.php
+++ app/Http/Controllers/api/ProjectController.php
@@ -9,7 +9,18 @@ use Illuminate\Http\Request;
 class ProjectController extends Controller
 {
     public function index(){
-        $project = Project::all();
+        $project = Project::with('skills')->get();
         return response()->json(['projects' => $project]);
     }
+
+    public function show($id)
+    {
+        $project = Project::with('skills')->find($id);
+        if (!$project) {
+            return response()->json(['message' => 'Progetto non trovato'], 404);
+        }
+
+        return response()->json(['project' => $project]);
+    }
+
 }
