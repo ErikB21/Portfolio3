a3d98583445e0b96b0a4f6c7f3ee8f257c8d0672
ErikBorgogno <erikborgo.20@gmail.com>
2024-12-30 12:59:19 +0100

    add dropdown

--- resources/js/views/app.vue
+++ resources/js/views/app.vue
@@ -2,10 +2,27 @@
     <div class="container-fluid d-flex justify-content-md-center align-items-md-center justify-content-lg-evenly bg_custom px-0 bg-dark">
         <div class="d-flex flex-column flex-md-wrap flex-md-row align-items-center justify-content-center justify-content-md-between justify-content-lg-evenly align-items-lg-center flex-lg-nowrap w-100">
             <AboutComponent class="d-none d-md-flex col-md-12 col-lg-3 order-md-1 mt-md-4 mt-lg-0" />
-            <div class="col-12 d-md-none order-1 justify-content-between align-items-center d-flex px-2 py-2">
-                <img src="/images/logo_white.png" alt="" />
-                <i class="fa-solid fa-bars fa-lg text-white"></i>
+            <div class="col-12 d-md-none order-1 d-flex justify-content-between align-items-center px-2 py-2">
+                <img src="/images/logo_white.png" alt="Logo" />
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">Contact</a>
+                        </li>
+                    </ul>
+                </div>
             </div>
+
             <MainComponent class="col-12 col-sm-11 col-md-10 col-lg-7 order-3 order-md-2 ms-md-3 ms-lg-0 my-md-2 my-lg-0" />
             <HeaderComponent class="col-12 col-sm-11 col-md-1 col-lg-1 order-2 order-md-3 my-3 my-md-0 me-md-3 me-lg-0 my-md-2 my-lg-0" />
         </div>
@@ -16,6 +33,8 @@
 import HeaderComponent from '../components/HeaderComponent.vue';
 import MainComponent from '../components/MainComponent.vue';
 import AboutComponent from '../components/AboutComponent.vue';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 export default {
     name: 'App',
